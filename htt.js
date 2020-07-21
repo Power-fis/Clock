0b002c7d364f0ce1b9a716ac1595ad3c381ecf5e
Power-fis <68431535+Power-fis@users.noreply.github.com>
2020-07-21 19:45:58 +0300

    clock circle

---
+++ htt.js
@@ -0,0 +1,18 @@
+let canvas = document.getElementById("sandbox"),
+	context = canvas.getContext("2D"),
+	square, circle;
+
+square = new Path2D();
+square.moveTo(0,0);
+square.lineTo(50,50);
+square.lineTo(50,250);
+square.lineTo(250,250);
+square.lineTo(250,50);
+square.lineTo(50,50);
+
+context.stroke(square);
+
+circle = new Path2D();
+circle.arc(150,150,100, 0 , 2 * Math.PI);
+
+context.fill(circle);
